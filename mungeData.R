a4b7459217efdc0b9863d8245fa0171a7e86c5c6
Chan Benjamin <Benjamin.Chan@dhsoha.state.or.us>
2020-07-02 10:37:50 -0700

    Chunk the reading of CRDT source file

--- mungeData.R
+++ mungeData.R
@@ -14,61 +14,87 @@ state <- read_csv(file.path("Data", "state_names.csv"))
 url <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vR_xmYt4ACPDZCDJcY12kCiMiH0ODyx3E1ZvgOHB8ae1tRcjXbs_yWBOA4j4uoCEADVfC1PS2jYO68B/pub?gid=43720681&single=true&output=csv"
 
 
-temp <-
+calculatePercent <- function(data) {
+  require(magrittr)
+  require(dplyr)
+  data %>%
+    mutate(Cases_Black_Pct = Cases_Black / Cases_Denom,
+           Cases_LatinX_Pct = Cases_LatinX / Cases_Denom,
+           Cases_Asian_Pct = Cases_Asian / Cases_Denom,
+           Cases_NHPI_Pct = Cases_NHPI / Cases_Denom,
+           Cases_AIAN_Pct = Cases_AIAN / Cases_Denom,
+           Cases_Multiracial_Pct = Cases_Multiracial / Cases_Denom,
+           Cases_White_Pct = Cases_White / Cases_Denom,
+           Cases_Other_Pct = Cases_Other / Cases_Denom,
+           Cases_Ethnicity_Hispanic_Pct = Cases_Ethnicity_Hispanic / (Cases_Ethnicity_Hispanic + Cases_Ethnicity_NonHispanic),
+           Cases_Ethnicity_NonHispanic_Pct = Cases_Ethnicity_NonHispanic / (Cases_Ethnicity_Hispanic + Cases_Ethnicity_NonHispanic),
+           Deaths_Black_Pct = Deaths_Black / Deaths_Denom,
+           Deaths_LatinX_Pct = Deaths_LatinX / Cases_Denom,
+           Deaths_Asian_Pct = Deaths_Asian / Deaths_Denom,
+           Deaths_NHPI_Pct = Deaths_NHPI / Deaths_Denom,
+           Deaths_AIAN_Pct = Deaths_AIAN / Deaths_Denom,
+           Deaths_Multiracial_Pct = Deaths_Multiracial / Deaths_Denom,
+           Deaths_White_Pct = Deaths_White / Deaths_Denom,
+           Deaths_Other_Pct = Deaths_Other / Deaths_Denom,
+           Deaths_Ethnicity_Hispanic_Pct = Deaths_Ethnicity_Hispanic / (Deaths_Ethnicity_Hispanic + Deaths_Ethnicity_NonHispanic),
+           Deaths_Ethnicity_NonHispanic_Pct = Deaths_Ethnicity_NonHispanic / (Deaths_Ethnicity_Hispanic + Deaths_Ethnicity_NonHispanic))
+}
+transpose <- function(data, suffix) {
+  require(magrittr)
+  require(dplyr)
+  data %>%
+    select(c(Date, State, ends_with(suffix))) %>%
+    pivot_longer(-c(Date, State)) %>%
+    mutate(name = gsub(suffix, "", name))
+}
+
+
+pct_ex_Unknown <-
   read_csv(url) %>%
-  mutate(Cases_Total_ex_Unknown = Cases_Total - Cases_Unknown,
-         Deaths_Total_ex_Unknown = Deaths_Total - Deaths_Unknown) %>%
-  mutate(Cases_Black_Pct = Cases_Black / Cases_Total_ex_Unknown,
-         Cases_LatinX_Pct = Cases_LatinX / Cases_Total_ex_Unknown,
-         Cases_Asian_Pct = Cases_Asian / Cases_Total_ex_Unknown,
-         Cases_NHPI_Pct = Cases_NHPI / Cases_Total_ex_Unknown,
-         Cases_AIAN_Pct = Cases_AIAN / Cases_Total_ex_Unknown,
-         Cases_Multiracial_Pct = Cases_Multiracial / Cases_Total_ex_Unknown,
-         Cases_White_Pct = Cases_White / Cases_Total_ex_Unknown,
-         Cases_Other_Pct = Cases_Other / Cases_Total_ex_Unknown,
-         Cases_Ethnicity_Hispanic_Pct = Cases_Ethnicity_Hispanic / (Cases_Ethnicity_Hispanic + Cases_Ethnicity_NonHispanic),
-         Cases_Ethnicity_NonHispanic_Pct = Cases_Ethnicity_NonHispanic / (Cases_Ethnicity_Hispanic + Cases_Ethnicity_NonHispanic),
-         Deaths_Black_Pct = Deaths_Black / Deaths_Total_ex_Unknown,
-         Deaths_LatinX_Pct = Deaths_LatinX / Cases_Total_ex_Unknown,
-         Deaths_Asian_Pct = Deaths_Asian / Deaths_Total_ex_Unknown,
-         Deaths_NHPI_Pct = Deaths_NHPI / Deaths_Total_ex_Unknown,
-         Deaths_AIAN_Pct = Deaths_AIAN / Deaths_Total_ex_Unknown,
-         Deaths_Multiracial_Pct = Deaths_Multiracial / Deaths_Total_ex_Unknown,
-         Deaths_White_Pct = Deaths_White / Deaths_Total_ex_Unknown,
-         Deaths_Other_Pct = Deaths_Other / Deaths_Total_ex_Unknown,
-         Deaths_Ethnicity_Hispanic_Pct = Deaths_Ethnicity_Hispanic / (Deaths_Ethnicity_Hispanic + Deaths_Ethnicity_NonHispanic),
-         Deaths_Ethnicity_NonHispanic_Pct = Deaths_Ethnicity_NonHispanic / (Deaths_Ethnicity_Hispanic + Deaths_Ethnicity_NonHispanic)) %>%
-  mutate(Cases_Black_small_denom_flag = as.logical(Cases_Black < 30),
-         Cases_LatinX_small_denom_flag = as.logical(Cases_LatinX < 30),
-         Cases_Asian_small_denom_flag = as.logical(Cases_Asian < 30),
-         Cases_NHPI_small_denom_flag = as.logical(Cases_NHPI < 30),
-         Cases_AIAN_small_denom_flag = as.logical(Cases_AIAN < 30),
-         Cases_Multiracial_small_denom_flag = as.logical(Cases_Multiracial < 30),
-         Cases_White_small_denom_flag = as.logical(Cases_White < 30),
-         Cases_Other_small_denom_flag = as.logical(Cases_Other < 30),
-         Cases_Ethnicity_Hispanic_small_denom_flag = as.logical(Cases_Ethnicity_Hispanic < 30),
-         Cases_Ethnicity_NonHispanic_small_denom_flag = as.logical(Cases_Ethnicity_NonHispanic < 30),
-         Deaths_Black_small_denom_flag = as.logical(Deaths_Black < 30),
-         Deaths_LatinX_small_denom_flag = as.logical(Deaths_LatinX < 30),
-         Deaths_Asian_small_denom_flag = as.logical(Deaths_Asian < 30),
-         Deaths_NHPI_small_denom_flag = as.logical(Deaths_NHPI < 30),
-         Deaths_AIAN_small_denom_flag = as.logical(Deaths_AIAN < 30),
-         Deaths_Multiracial_small_denom_flag = as.logical(Deaths_Multiracial < 30),
-         Deaths_White_small_denom_flag = as.logical(Deaths_White < 30),
-         Deaths_Other_small_denom_flag = as.logical(Deaths_Other < 30),
-         Deaths_Ethnicity_Hispanic_small_denom_flag = as.logical(Deaths_Ethnicity_Hispanic < 30),
-         Deaths_Ethnicity_NonHispanic_small_denom_flag = as.logical(Deaths_Ethnicity_NonHispanic < 30)) %>%
-  select(c(Date, State, ends_with("Pct"), ends_with("flag")))
+  mutate(Cases_Denom = Cases_Total - Cases_Unknown,
+         Deaths_Denom = Deaths_Total - Deaths_Unknown) %>%
+  calculatePercent() %>%
+  transpose("_Pct") %>%
+  rename(percent = value)
+
+pct_incl_Unknown <-
+  read_csv(url) %>%
+  mutate(Cases_Denom = Cases_Total,
+         Deaths_Denom = Deaths_Total) %>%
+  calculatePercent() %>%
+  transpose("_Pct") %>%
+  rename(percent_incl_Unknown = value)
+
+small_numer_flag <-
+  read_csv(url) %>%
+  mutate(Cases_Black_small_numer_flag = as.logical(Cases_Black < 30),
+         Cases_LatinX_small_numer_flag = as.logical(Cases_LatinX < 30),
+         Cases_Asian_small_numer_flag = as.logical(Cases_Asian < 30),
+         Cases_NHPI_small_numer_flag = as.logical(Cases_NHPI < 30),
+         Cases_AIAN_small_numer_flag = as.logical(Cases_AIAN < 30),
+         Cases_Multiracial_small_numer_flag = as.logical(Cases_Multiracial < 30),
+         Cases_White_small_numer_flag = as.logical(Cases_White < 30),
+         Cases_Other_small_numer_flag = as.logical(Cases_Other < 30),
+         Cases_Ethnicity_Hispanic_small_numer_flag = as.logical(Cases_Ethnicity_Hispanic < 30),
+         Cases_Ethnicity_NonHispanic_small_numer_flag = as.logical(Cases_Ethnicity_NonHispanic < 30),
+         Deaths_Black_small_numer_flag = as.logical(Deaths_Black < 30),
+         Deaths_LatinX_small_numer_flag = as.logical(Deaths_LatinX < 30),
+         Deaths_Asian_small_numer_flag = as.logical(Deaths_Asian < 30),
+         Deaths_NHPI_small_numer_flag = as.logical(Deaths_NHPI < 30),
+         Deaths_AIAN_small_numer_flag = as.logical(Deaths_AIAN < 30),
+         Deaths_Multiracial_small_numer_flag = as.logical(Deaths_Multiracial < 30),
+         Deaths_White_small_numer_flag = as.logical(Deaths_White < 30),
+         Deaths_Other_small_numer_flag = as.logical(Deaths_Other < 30),
+         Deaths_Ethnicity_Hispanic_small_numer_flag = as.logical(Deaths_Ethnicity_Hispanic < 30),
+         Deaths_Ethnicity_NonHispanic_small_numer_flag = as.logical(Deaths_Ethnicity_NonHispanic < 30)) %>%
+  transpose("_small_numer_flag") %>%
+  rename(small_numer_flag = value)
   
+
 crdt <-
-  inner_join(temp %>%
-               select(c(Date, State, ends_with("Pct"))) %>%
-               pivot_longer(-c(Date, State), values_to = "percent") %>%
-               mutate(name = gsub("_Pct", "", name)),
-             temp %>%
-               select(c(Date, State, ends_with("small_denom_flag"))) %>%
-               pivot_longer(-c(Date, State), values_to = "small_denom_flag") %>%
-               mutate(name = gsub("_small_denom_flag", "", name))) %>%
+  pct_ex_Unknown %>%
+  inner_join(small_numer_flag) %>%
+  inner_join(pct_incl_Unknown) %>%
   mutate(metric = case_when(grepl("Cases", name) ~ "Cases",
                             grepl("Deaths", name) ~ "Deaths")) %>%
   mutate(category = gsub("(Cases|Deaths)_", "", name)) %>%
@@ -87,7 +113,7 @@ crdt <-
   mutate(category = case_when(variable == "Ethnicity" & category == "Hispanic" ~ "Hispanic or Latino (of any race)",
                               variable == "Ethnicity" & category == "Not Hispanic" ~ "Not Hispanic or Latino",
                               TRUE ~ category)) %>%
-  select(Date, State, metric, variable, category, percent, small_denom_flag) %>%
+  select(Date, State, metric, variable, category, percent, percent_incl_Unknown, small_numer_flag) %>%
   inner_join(state, by = c("State" = "State_Abbr") ) %>%
   mutate(oregon_flag = as.logical(State == "OR")) %>%
   mutate(tooltip_text = sprintf("of COVID19 %s in %s were from individuals of %s %s (as of %s)",
@@ -95,7 +121,8 @@ crdt <-
                                 State_Name,
                                 category,
                                 tolower(variable),
-                                Date %>% as.character() %>% as.Date(format = "%Y%m%d") %>% format("%B %d, %Y")))
+                                Date %>% as.character() %>% as.Date(format = "%Y%m%d") %>% format("%B %d, %Y"))) %>%
+  select(Date, State, State_Name, everything())
 
 crdt %>% write_csv(file.path("Data", "CRDT.csv"), na = "")
 
