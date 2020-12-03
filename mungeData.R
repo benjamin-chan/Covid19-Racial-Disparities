c7290cf9cc77e73ac9516a00e16614811f54dddd
Chan Benjamin <Benjamin.Chan@dhsoha.state.or.us>
2020-12-03 08:19:25 -0800

    Use updated URL

--- mungeData.R
+++ mungeData.R
@@ -36,7 +36,7 @@ reporting_characteristics <-
 # The COVID Tracking Projects's Racial Data Dashboard
 # https://covidtracking.com/race/dashboard
 readCRDT <- function () {
-  url <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vR_xmYt4ACPDZCDJcY12kCiMiH0ODyx3E1ZvgOHB8ae1tRcjXbs_yWBOA4j4uoCEADVfC1PS2jYO68B/pub?gid=43720681&single=true&output=csv"
+  url <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vS8SzaERcKJOD_EzrtCDK1dX1zkoMochlA9iHoHg_RSw3V8bkpfk1mpw4pfL5RdtSOyx_oScsUtyXyk/pub?gid=43720681&single=true&output=csv"
   require(magrittr)
   require(dplyr)
   require(readr)
